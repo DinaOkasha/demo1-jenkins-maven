6da882918da38efbca03ebc843c9c486d45766d0
Dina <144805635+DinaOkasha@users.noreply.github.com>
2024-09-10 16:54:36 +0300

    adding new person Dina

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -39,6 +39,13 @@ public class PersonService {
 		p.setFirstName("AbdulHalim");
 		p.setLastName("Mahmoud");
 		persons.add(p);
+
+                p = new Person();
+		p.setId("5");
+		p.setAge(30);
+		p.setFirstName("Dina_webhock");
+		p.setLastName("Magdy");
+		persons.add(p);
 		
 	}
 	public Person getPerson(String id) {
